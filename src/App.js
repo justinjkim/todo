ff9cf6ebbfa53d6c134ce553692d34c816d3f932
Justin Kim <jjkim928@gmail.com>
2019-02-06 16:05:18 -0800

    add general components, didn't build them yet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      
+      <div className="wrapper">
+      <Title />
+      <TodoForm />
+      <TodoList />
+      </div> 
     );
   }
 }
