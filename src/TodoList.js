23ee3104f1625d7d256b88c2776eba5a732cada9
Justin Kim <jjkim928@gmail.com>
2019-02-06 16:50:48 -0800

    refer to props

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,7 +4,13 @@ class TodoList extends React.Component {
 	render() {
 		return(
 			<form onSubmit={this.props.addItem}>
-				<input type="text" placeholder="Buy milk..." />
+				<input 
+					type="text" 
+					placeholder="Buy milk..."
+					ref={this.props.inputElement}
+					value={this.props.currentItem.text}
+					onChange={this.props.handleInputChange} 
+				/>
 				<button type="submit">Add Task</button>
 			</form>
 		);
