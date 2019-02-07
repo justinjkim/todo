83853bcf52bb80711c0a9b1a1fdc8a51eb8c227e
Justin Kim <jjkim928@gmail.com>
2019-02-06 16:31:22 -0800

    get rid of TodoForm component, gonna put into TodoList component for simplicity sake

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
-import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 class App extends Component {
@@ -13,11 +12,15 @@ class App extends Component {
     }
   }
 
+  addItem = (event) => {
+    event.preventDefault();
+    console.log('added item!');
+  }
+
   render() {
     return (
       <div className="wrapper">
         <Title />
-        <TodoForm />
         <TodoList />
       </div> 
     );
