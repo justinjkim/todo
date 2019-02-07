30d8403986ba072b8550991e70c3b18c1b925921
Justin Kim <jjkim928@gmail.com>
2019-02-06 16:12:54 -0800

    formatting changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './Title';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
-      <Title />
-      <TodoForm />
-      <TodoList />
+        <Title />
+        <TodoForm />
+        <TodoList />
       </div> 
     );
   }
